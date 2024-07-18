b53b8e59b3009796a2fd9409e427adcb58001471
Fausto Calviño <faustocalvino@outlook.com>
2024-07-18 16:14:23 +0200

    actualizando estilos y funcionalidad de dos idiomas

--- src/data/english.js
+++ src/data/english.js
@@ -1,3 +1,24 @@
-const Navbar = {
-    
-}
+export const HERO_EN = [
+  "Available to work",
+  "Hi, i'm Fausto",
+  "A web developer with three years of experience in the ecosystem of applications developed with React, Next.js, and backend applications based on NodeJS.",
+  "Email",
+];
+
+export const EXPERIENCIA_EN = [
+  "Experience",
+  "May 2022 - April 2023",
+  "WordPress Developer",
+  "I have one year of experience developing self-managed websites with WordPress, creating landing pages, e-commerce sites with WooCommerce, and news portals. I use Elementor to design attractive and functional interfaces, making it easy for my clients to manage and update their websites.",
+  "July 2021 - Nowadays",
+  "Adding knowledge about web development",
+  "Since July 2021, I have been deeply involved in web development, learning both backend and frontend. This time has been very important for me, as I have been able to improve significantly and learn about various technologies and tools.",
+];
+export const SOBREMI_EN = [
+  "About me",
+  "Since mid-2021, I have been learning web development in a self-taught manner. I have solid knowledge in HTML, CSS, JavaScript, as well as in the ecosystem of frameworks such as React and Next.js.",
+  "Currently, I am expanding my skills in Node.js, Go, and databases. You can learn more about my projects and experience on my web portfolio.",
+  "I am available for job opportunities, both remote and hybrid, in the city of Madrid, and I am excited to collaborate on projects involving these technologies.",
+  "about-me"
+];
+const NAVBAR_EN=["Experience","About me","Projects","Contact"]
---
+++ src/data/espanol.js
@@ -0,0 +1,26 @@
+export const HERO_ES = [
+  "Disponible para trabajar",
+  "Hola, soy Fausto",
+  "Un desarrollador web con tres años de experiencia en el ecosistema de aplicaciones desarrolladas con React, Next.js y aplicaciones de backend basadas en NodeJS.",
+  "Correo",
+];
+
+export const EXPERIENCIA_ES = [
+  "Experiencia",
+  "Mayo 2022 - Abril 2023",
+  "WordPress Developer",
+  "Tengo un año de experiencia desarrollando sitios web autogestionables con WordPress, creando landing pages, e-commerce con WooCommerce y portales de noticias. Utilizo Elementor para diseñar interfaces atractivas y funcionales, facilitando a mis clientes la gestión y actualización de sus sitios web.",
+  "Julio 2021 - Actualmente",
+  "Sumando conocimientos sobre el desarrollo web",
+  "Desde julio de 2021, he estado metido de lleno en el desarrollo web, aprendiendo tanto backend como frontend. Este tiempo ha sido súper importante para mí, ya que he podido mejorar mucho y aprender sobre varias tecnologías y herramientas.",
+];
+
+export const SOBREMI_ES = [
+  "Sobre mí",
+  "Desde mediados de 2021, he estado aprendiendo desarrollo web de manera autodidacta. Poseo sólidos conocimientos en HTML, CSS, JavaScript, así como en el ecosistema de los frameworks React y Next.js.",
+  "Actualmente, estoy ampliando mis habilidades en Node.js, Go y bases de datos. Puedes conocer más sobre mis proyectos y experiencia en mi portfolio web.",
+  "Estoy disponible para oportunidades laborales, tanto remotas como híbridas, en la ciudad de Madrid, y estoy entusiasmado por colaborar en proyectos que involucren estas tecnologías.",
+  "sobre-mi"
+];
+
+const NAVBAR_ES = ["Experiencia", "Sobre mí", "Proyectos", "Contacto"];

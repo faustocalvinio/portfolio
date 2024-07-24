0d442279950242fdb1bf1c599d66a202cb06a1e5
Fausto Calviño <faustocalvino@outlook.com>
2024-07-24 15:21:12 +0200

    cambiando meta tags segun idioma

---
+++ src/config-en.ts
@@ -0,0 +1,8 @@
+export const SITE_TITLE = "Fausto Calviño's Portfolio - Web Developer";
+export const SITE_DESCRIPTION =
+  "Explore the portfolio of Fausto Calviño, a web developer and programmer specializing in front-end and back-end technologies. Discover my projects, skills in HTML, CSS, JavaScript, React, Next.js, Node.js, and Go, and how I can help your team create innovative and efficient web solutions.";
+export const TWITTER_HANDLE = "@faustocalvinio";
+export const MY_NAME = "Fausto Calviño";
+
+const BASE_URL = new URL("https://faustocalvinio.website/");
+export const SITE_URL = BASE_URL.origin;
--- src/config-es.ts
+++ src/config-es.ts
@@ -3,15 +3,6 @@ export const SITE_DESCRIPTION =
   "Explora el portfolio de Fausto Calviño, un desarrollador y programador web especializado en tecnologías front-end y back-end. Descubre mis proyectos, habilidades en HTML, CSS, JavaScript, React, Next.js, Node.js y Go, y cómo puedo ayudar a tu equipo a crear soluciones web innovadoras y eficientes.";
 export const TWITTER_HANDLE = "@faustocalvinio";
 export const MY_NAME = "Fausto Calviño";
-const siteUrl = `${import.meta.env.SITE}`;
-console.log(siteUrl)
-if (!siteUrl) {
-  console.log(siteUrl)
-  throw new Error('La variable de entorno SITE no está definida.');
-}
 
-const BASE_URL = new URL("https://faustocalvinio.netlify.app/");
+const BASE_URL = new URL("https://faustocalvinio.website/");
 export const SITE_URL = BASE_URL.origin;
-
-// const BASE_URL = new URL(import.meta.env.SITE);
-// export const SITE_URL = BASE_URL.origin;

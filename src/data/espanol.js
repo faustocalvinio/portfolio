d2f876f17daefe492d841aceda64fb935fc339cb
Fausto Calviño <faustocalvino@outlook.com>
2025-09-05 18:48:28 +0200

    feat: actualizar descripciones y enlaces en secciones de experiencia y proyectos

--- src/data/espanol.js
+++ src/data/espanol.js
@@ -14,7 +14,7 @@ export const EXPERIENCIA_ES = [
   "Tengo más de dos años de experiencia desarrollando sitios web autogestionables con WordPress, creando landing pages, e-commerce con WooCommerce y portales de noticias. Utilizo Elementor para diseñar interfaces atractivas y funcionales, facilitando a mis clientes la gestión y actualización de sus sitios web.",
   "Julio 2021 - Actualmente",
   "Sumando conocimientos sobre el desarrollo web",
-  "Desde julio de 2021, he estado metido de lleno en el desarrollo web, aprendiendo tanto backend como frontend. Este tiempo ha sido súper importante para mí, ya que he podido mejorar mucho y aprender sobre varias tecnologías y herramientas.",
+  "Desde julio de 2021, me he especializado en el desarrollo web full-stack. Durante este período he consolidado mis competencias técnicas, ya que he podido mejorar mucho y aprender sobre varias tecnologías y herramientas.",
   "Ver presentación de proyectos",
   "proyectos-wordpress",
   "Ver conocimientos detallados",
@@ -25,7 +25,7 @@ export const SOBREMI_ES = [
   "Sobre mí",
   "Desde mediados de 2021, he estado aprendiendo desarrollo web de manera autodidacta. Poseo sólidos conocimientos en HTML, CSS, JavaScript, así como en el ecosistema de los frameworks React y Next.js.",
   "Actualmente, estoy ampliando mis habilidades en Node.js, Go y bases de datos. Puedes conocer más sobre mis proyectos y experiencia en mi portfolio web.",
-  "Estoy disponible para oportunidades laborales, tanto remotas como híbridas, en la ciudad de Madrid, y estoy entusiasmado por colaborar en proyectos que involucren estas tecnologías.",
+  "Disponible para nuevos proyectos y oportunidades de colaboración. Contacta conmigo para discutir cómo puedo contribuir a tu equipo.",
   "sobre-mi",
 ];
 
